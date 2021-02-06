9ef5060a485e104b98d08efb336bb4615871ae77
kusuma <erlanggajatikusuma007@gmail.com>
2021-02-06 21:40:42 +0700

    add modal(no style)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './home.css';
 import {useSelector} from 'react-redux';
 import { useHistory } from "react-router-dom";
+import Modal from 'react-modal';
 
+
+const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)',
+      width                 : 550
+    }
+  };
 
 function Home() {
     const products = useSelector(state => state.products);
     let history = useHistory();
+    const [modalIsOpen,setIsOpen] = useState(false);
+
+    const handleToCart = (product) => {
+        setIsOpen(true)
+        console.log(product)
+    }
+
     return (
         <div className="wrapper">
             {
@@ -16,9 +36,14 @@ function Home() {
                         <img src={product.image} width={255} height={250} alt="product" />
                         <div className="title">{product.name}</div>
                         <h4>Rp. {product.price}</h4>
-                        <button onClick={() => console.log(product)}>Add to cart</button>
+                        {/* <button onClick={() => console.log(product)}>Add to cart</button> */}
+                        <button onClick={handleToCart}>Add to cart</button>
                     </div>)
             }
+            <Modal isOpen={modalIsOpen} style={customStyles}>
+                <h2>Modal Title</h2>
+                <div onClick={() => setIsOpen(false)}>X</div>
+            </Modal>
             
         </div>
     )
